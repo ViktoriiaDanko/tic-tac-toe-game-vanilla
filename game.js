39ff9482e159e93a44276dc6ccf947a0ca2b6ad1
ViktoriaDanko <danko.vik@mail.ru>
2019-06-06 12:00:35 +0300

    game

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ window.onload = function() {
     
     document.querySelector('.game').onclick = function(event) {
         if (event.target.className === 'block') {
-            number = number % 2 === 0 ? event.target.innerHTML = 'X' : event.target.innerHTML = '0';
+           return number % 2 ===  0 ? 'X' : '0';
         }
         gameResult();
         }
