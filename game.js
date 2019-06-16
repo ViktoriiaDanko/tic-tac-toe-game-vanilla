0b3589fa1ad6117a2d8c6722bb1f0b27889a68d5
ViktoriaDanko <danko.vik@mail.ru>
2019-06-16 17:37:52 +0300

    tic tac toe

--- game.js
+++ game.js
@@ -16,23 +16,23 @@ window.onload = function() {
 
         function getGameStatus() {
             let allItems = document.getElementsByClassName('block');
-            if (allItems[0].innerHTML === 'X' && allItems[1].innerHTML === 'X' && allItems[2].innerHTML === 'X') alert('Выиграли крестики');
-            if (allItems[3].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[5].innerHTML === 'X') alert('Выиграли крестики');
-            if (allItems[6].innerHTML === 'X' && allItems[7].innerHTML === 'X' && allItems[8].innerHTML === 'X') alert('Выиграли крестики');
-            if (allItems[0].innerHTML === 'X' && allItems[3].innerHTML === 'X' && allItems[6].innerHTML === 'X') alert('Выиграли крестики');
-            if (allItems[1].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[7].innerHTML === 'X') alert('Выиграли крестики');
-            if (allItems[2].innerHTML === 'X' && allItems[5].innerHTML === 'X' && allItems[8].innerHTML === 'X') alert('Выиграли крестики');
-            if (allItems[0].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[8].innerHTML === 'X') alert('Выиграли крестики'); 
-            if (allItems[2].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[6].innerHTML === 'X') alert('Выиграли крестики');
+            if (allItems[0].innerHTML === 'X' && allItems[1].innerHTML === 'X' && allItems[2].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
+            if (allItems[3].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[5].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
+            if (allItems[6].innerHTML === 'X' && allItems[7].innerHTML === 'X' && allItems[8].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
+            if (allItems[0].innerHTML === 'X' && allItems[3].innerHTML === 'X' && allItems[6].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
+            if (allItems[1].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[7].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
+            if (allItems[2].innerHTML === 'X' && allItems[5].innerHTML === 'X' && allItems[8].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
+            if (allItems[0].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[8].innerHTML === 'X') return {isGameEnded: true, winner: 'X'}; 
+            if (allItems[2].innerHTML === 'X' && allItems[4].innerHTML === 'X' && allItems[6].innerHTML === 'X') return {isGameEnded: true, winner: 'X'};
 
-            if (allItems[0].innerHTML === '0' && allItems[1].innerHTML === '0' && allItems[2].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[3].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[5].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[6].innerHTML === '0' && allItems[7].innerHTML === '0' && allItems[8].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[0].innerHTML === '0' && allItems[3].innerHTML === '0' && allItems[6].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[1].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[7].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[2].innerHTML === '0' && allItems[5].innerHTML === '0' && allItems[8].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[0].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[8].innerHTML === '0') alert('Выиграли нолики');
-            if (allItems[2].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[6].innerHTML === '0') alert('Выиграли нолики');
+            if (allItems[0].innerHTML === '0' && allItems[1].innerHTML === '0' && allItems[2].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[3].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[5].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[6].innerHTML === '0' && allItems[7].innerHTML === '0' && allItems[8].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[0].innerHTML === '0' && allItems[3].innerHTML === '0' && allItems[6].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[1].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[7].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[2].innerHTML === '0' && allItems[5].innerHTML === '0' && allItems[8].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[0].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[8].innerHTML === '0') return {isGameEnded: true, winner: '0'};
+            if (allItems[2].innerHTML === '0' && allItems[4].innerHTML === '0' && allItems[6].innerHTML === '0') return {isGameEnded: true, winner: '0'};
         }
 }
 
